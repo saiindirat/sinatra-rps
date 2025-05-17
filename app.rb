42df39f7cf981ed531b591a76589e92b4eab7429
saiindirat <tholusaiindira@gmail.com>
2025-05-17 21:54:30 +0000

    complete RPS Sinatra project

--- app.rb
+++ app.rb
@@ -1,9 +1,51 @@
 require "sinatra"
-require "sinatra/reloader"
+require "sinatra/reloader" if development?
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:index)
+end
+
+get("/rock") do
+  moves = ["rock", "paper", "scissors"]
+  @computer_move = moves.sample
+
+  if @computer_move == "rock"
+    @outcome = "We tied!"
+  elsif @computer_move == "paper"
+    @outcome = "We lost!"
+  else
+    @outcome = "We won!"
+  end
+
+  erb(:move)
+end
+
+get("/paper") do
+  moves = ["rock", "paper", "scissors"]
+  @computer_move = moves.sample
+
+  if @computer_move == "rock"
+    @outcome = "We won!"
+  elsif @computer_move == "paper"
+    @outcome = "We tied!"
+  else
+    @outcome = "We lost!"
+  end
+
+  erb(:move)
+end
+
+get("/scissors") do
+  moves = ["rock", "paper", "scissors"]
+  @computer_move = moves.sample
+
+  if @computer_move == "rock"
+    @outcome = "We lost!"
+  elsif @computer_move == "paper"
+    @outcome = "We won!"
+  else
+    @outcome = "We tied!"
+  end
+
+  erb(:move)
 end
